55ea3669abdce3ca147137afbd4665a12929cb5a
Mandeep Kaur <Mandeep.Kaur1@globalpueblosolutions.com>
2016-07-08 19:24:02 +0530

    first commit

---
+++ Angular2/app/app.component.js
@@ -0,0 +1,36 @@
+﻿
+(function (app) {
+
+    app.AppComponent =
+
+      ng.core.Component({
+          selector: 'TaskContainer',
+          template: '<h1>Task Manager</h1>' +
+              '<div><input type="text" required id="txtTask" [(ngModel)]="taskname" placeholder=" Enter task name"/>' +
+              '<input type="text" required id="txtTaskDetail" [(ngModel)]="taskdetail" placeholder="Enter task detail"/>' +
+              '<button (click)="AddTask()">Add</button></div>' +
+              '{{clickMessage}}' +
+              '<table><tr *ngFor="let task of tasksList" >' +
+              '<td>{{task.name}}</td><td><button (click)="onSelect(task)">Show Detail</button></td></tr></table>' +
+              '<div *ngIf="selectedTask">'+
+              '<h2>Task details:</h2>' +
+              '<div>{{selectedTask.detail}}</div>' +
+               '</div>'
+          })
+      .Class({
+          constructor: function () {
+              this.clickMessage = 'After button click add tasks here';
+              this.tasksList = [];
+          },
+          AddTask(event) {
+              this.clickMessage = 'Add task click Clicked';
+              this.tasksList.push({ name: this.taskname, detail: this.taskdetail });
+              this.taskname = "";
+              this.taskdetail = "";
+          },
+          onSelect(task) { this.selectedTask = task; }
+
+      })
+
+
+})(window.app || (window.app = {}));
